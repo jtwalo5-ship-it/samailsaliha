1c27215b1d0b653f49b70f32172f5f60b4370964
Justice Okeke <jtwalo5@gmail.com>
2025-11-10 00:52:56 -0500

    Initial working version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>VETRON</h1>
+      <p>Engineering Tomorrow’s Drive</p>
+      <button>Configure Your Car</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/quote.js
@@ -0,0 +1,4 @@
+const quote = "Your Business Is Our Business.";
+console.log("Hello from quote.js!");
+console.log("Quote:", quote);
+
